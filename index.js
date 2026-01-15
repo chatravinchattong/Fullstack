372c3b3ee1c78b7af705c9717ea4727b96a24f34
chatravinchattong <chatravinfield@gmail.com>
2026-01-15 16:02:30 +0700

    intial

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+        });
